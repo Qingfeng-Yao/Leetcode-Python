e2de01e126555f304878aaa990f5026623c051a7
Qingfeng-Yao <18801483261@163.com>
2021-01-05 18:33:10 +0800

    add 1672 and 1673

--- README.md
+++ README.md
@@ -1,1 +1,3 @@
-## Leetcode-Python
+## array
+- 考虑修改已有数组为己用
+- 子序列: 遍历+pop/while
---
+++ array/1672.-Richest-Customer-Wealth.py
@@ -0,0 +1,19 @@
+'''
+Problem Discription:
+You are given an m x n integer grid accounts where accounts[i][j] is the amount of money the i​​​​​​​​​​​th​​​​ customer has in the j​​​​​​​​​​​th​​​​ bank. Return the wealth that the richest customer has.
+A customer's wealth is the amount of money they have in all their bank accounts. The richest customer is the customer that has the maximum wealth.
+
+Example:
+Input: accounts = [[1,2,3],[3,2,1]]
+Output: 6
+'''
+
+## self solution
+### 一层循环，使用已有数组空间
+## Runtime: 77.95%; Memory Usage: 85.02%
+def maximumWealth(self, accounts: List[List[int]]) -> int:
+    for c in accounts:
+        if sum(c) > accounts[0][0]:
+            accounts[0][0] = sum(c)
+            
+    return accounts[0][0]
