03f428199428d8db52f3d8cf3ecaa02a3021aaaf
Qingfeng-Yao <18801483261@163.com>
2021-01-08 10:05:57 +0800

    add 1680

---
+++ integer/1680M-Concatenation-of-Consecutive-Binary-Numbers.py
@@ -0,0 +1,17 @@
+'''
+Problem Discription:
+Given an integer n, return the decimal value of the binary string formed by concatenating the binary representations of 1 to n in order, modulo 10^9 + 7.
+
+Example:
+Input: n = 3
+Output: 27
+'''
+
+## self solution
+### bin函数+int函数
+def concatenatedBinary(self, n: int) -> int:
+    c = ""
+    for i in range(1, n+1):
+        c += bin(i)[2:]
+        
+    return int(c, 2)%(10**9+7)
